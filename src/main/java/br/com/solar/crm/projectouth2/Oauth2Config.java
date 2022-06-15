2e9880ab3f619855f16b4f846d52d2a0f6b4bd4d
Jeferson Queiroga <jefersonqueiroga@gmail.com>
2022-06-15 19:13:14 -0300

    Adicionando novas alterações.

--- src/main/java/br/com/solar/crm/projectouth2/Oauth2Config.java
+++ src/main/java/br/com/solar/crm/projectouth2/Oauth2Config.java
@@ -3,32 +3,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @EnableWebSecurity
-public class Oauth2Config extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {// @formatter:off
-        http.authorizeRequests()
-                .antMatchers("/", "/oauth/**","/api/**")
-                .permitAll()
-                .anyRequest()
+public class Oauth2Config{
+
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http.authorizeExchange()
+                .anyExchange()
                 .authenticated()
                 .and()
                 .oauth2Login();
+        return http.build();
     }
 
-    @Bean
-    WebClient webClient(ClientRegistrationRepository clientRegistrationRepository,
-                        OAuth2AuthorizedClientRepository authorizedClientRepository) {
-        ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2 =
-                new ServletOAuth2AuthorizedClientExchangeFilterFunction(clientRegistrationRepository,
-                        authorizedClientRepository);
-        oauth2.setDefaultOAuth2AuthorizedClient(true);
-        System.out.println(oauth2.oauth2Configuration());
-        return WebClient.builder().apply(oauth2.oauth2Configuration()).build();
-    }
 }
