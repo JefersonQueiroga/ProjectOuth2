18906a430cffa86d9c5c0dad754daf1541514ea9
jefersonqueiroga <jefersonqueiroga@gmail.com>
2022-06-15 22:36:39 -0300

    Versão instável

--- src/main/java/br/com/solar/crm/projectouth2/controller/TesteController.java
+++ src/main/java/br/com/solar/crm/projectouth2/controller/TesteController.java
@@ -1,6 +1,8 @@
 package br.com.solar.crm.projectouth2.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -8,6 +10,7 @@ import reactor.core.publisher.Mono;
 
 @RestController
 public class TesteController {
+
 
     @Autowired
     private WebClient webClient;
@@ -19,10 +22,12 @@ public class TesteController {
 
     @GetMapping("/api/teste2")
     Mono<String> useOauthWithAuthCode() {
-        Mono<String> retrievedResource = webClient.get()
+        Mono<String> retrievedResource = this.webClient.get()
                 .uri("https://api.pipedrive.com/v1/users/me")
                 .retrieve()
                 .bodyToMono(String.class);
+
+        System.out.println("--------------------------------------##########################");
         return retrievedResource.map(string ->
                 "We retrieved the following resource using Oauth: " + string);
     }
